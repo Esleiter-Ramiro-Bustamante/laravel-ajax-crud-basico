4029b44daefcc594f80ee8ccc3853512209bba37
Esleiter Ramiro Bustamante <esletutuns0307@gmail.com>
2018-02-17 01:17:28 -0600

    Version 1.0

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('#alert').hide();
+	$('.btn-delete').click(function(e){
+		e.preventDefault();
+		if (!confirm("¿Esta seguro de eliminar?")) {
+			return false;
+		}
+		var row =$(this).parents('tr');
+		var form=$(this).parents('form');
+		var url=form.attr('action');
+		$('#alert').show();
+		$.post(url,form.serialize(),function(result){
+			row.fadeOut();
+			$('#products-total').html(result.total);
+			$('#alert').html(result.message);
+		}).fail(function(){
+			$('#alert').html('Algo salio mal');
+		});
+		});
+	});
